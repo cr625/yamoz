4816739d89d1e28a86e517b1b9333525168b466c
cr625 <cr625@drexel.edu>
2024-11-21 18:30:43 -0500

    lookup the term_string and definition by concept_id

--- app/term/views/api_routes.py
+++ app/term/views/api_routes.py
@@ -2,7 +2,8 @@ from flask import jsonify
 from app.term import term_blueprint as term
 from app.term.models import Term
 
-term.route("/api/<concept_id>")
+
+@term.route("/api/<concept_id>")
 def get_term_by_concept_id(concept_id):
     term = Term.query.filter_by(concept_id=concept_id).first()
     if term:
