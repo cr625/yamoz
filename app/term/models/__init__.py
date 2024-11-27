9e3ff7b8906a5e170b595681d8b3e1d5f06bcb24
cr625 <cr625@drexel.edu>
2024-11-27 08:46:43 -0500

    organize term models

---
+++ app/term/models/__init__.py
@@ -0,0 +1,3 @@
+from .term import Term, TermSet, Tag, Track, Vote, Comment, TermSet, term_saved, term_updated, term_deleted, term_tracked
+from .ark import Ark
+from .relationship import Relationship
---
+++ app/term/models/relationship.py
@@ -0,0 +1,49 @@
+from app import db
+from sqlalchemy.ext.hybrid import hybrid_property
+from rdflib import URIRef
+
+
+class Relationship(db.Model):
+    __tablename__ = "relationships"
+    id = db.Column(db.Integer, primary_key=True)
+    ark_id = db.Column(db.Integer, db.ForeignKey("arks.id"), nullable=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    created = db.Column(db.DateTime, default=db.func.now())
+    modified = db.Column(db.DateTime, default=db.func.now(),
+                         onupdate=db.func.now())
+    parent_id = db.Column(
+        db.Integer, db.ForeignKey("terms.id"), nullable=False)
+    child_id = db.Column(db.Integer, db.ForeignKey("terms.id"), nullable=False)
+    predicate_id = db.Column(
+        db.Integer, db.ForeignKey("terms.id"), nullable=False)
+    namespace = db.Column(db.String(128), nullable=True)
+    timestamp = db.Column(db.DateTime, default=db.func.now())
+
+    # Establish relationships
+    predicate = db.relationship(
+        "Term", foreign_keys=[predicate_id], backref="relationships_as_predicate")
+
+    ark = db.relationship("Ark", foreign_keys=[
+                          ark_id], backref="relationships")
+
+    @hybrid_property
+    def ark_concept_id(self):
+        if self.ark:
+            return self.ark.concept_id
+        return None
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def get_predicate_uri(self):
+        if self.namespace:
+            return URIRef(f"{self.namespace}{self.predicate.term_string}")
+        return URIRef(self.predicate.term_string)
+
+    def __repr__(self):
+        return f"<Relationship {self.parent_id} {self.predicate.term_string} {self.child_id}>"
