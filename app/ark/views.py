827489874e4b0112836716551794053dbe92fdf1
cr625 <cr625@drexel.edu>
2024-12-04 08:14:32 -0500

    add standalone instance

--- app/ark/views.py
+++ app/ark/views.py
@@ -2,7 +2,9 @@ from flask import redirect, url_for, render_template
 
 from app.ark import ark_blueprint as ark
 
+
 @ark.route("/")
+@ark.route(":/99152/<concept_id>")
 @ark.route("/99152/<concept_id>")
 @ark.route("/<concept_id>")
 def index(concept_id=None):
@@ -11,4 +13,6 @@ def index(concept_id=None):
     return redirect(url_for("term.display_term", concept_id=concept_id))
 
 
-   
+@ark.route("/13183/<concept_id>")
+def show_relations():
+    pass
--- app/io/ontology.py
+++ app/io/ontology.py
@@ -29,8 +29,24 @@ class OwlHandler:
         return terms
 
 
-# Example usage:
-# handler = OwlHandler("app/io/import/bfo.owl")
-# terms = handler.get_ontology_terms()
-# for term in terms:
-#    print(term)
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
+
+    Base = declarative_base()
+    engine = create_engine('postgresql://postgres:PASS@localhost/yamz-o')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    class User(Base):
+        __tablename__ = 'users'
+        __table_args__ = {'autoload': True, 'autoload_with': engine}
+
+    class Term(Base):
+        __tablename__ = 'terms'
+        __table_args__ = {'autoload': True, 'autoload_with': engine}
+
+    def get_user_by_email(email):
+        return session.query(User).filter_by(email=email).first()
