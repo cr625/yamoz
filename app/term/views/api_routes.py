2d6546320cfd344cf0d8863547864a68eeb4959b
cr625 <cr625@drexel.edu>
2024-12-05 17:47:41 -0500

    update modal add subclass to look up term

--- app/term/views/api_routes.py
+++ app/term/views/api_routes.py
@@ -12,3 +12,16 @@ def get_term_by_concept_id(concept_id):
             "definition": term.definition
         })
     return jsonify({"error": "Term not found"}), 404
+
+
+@term.route('/api/name/<term_name>', methods=['GET'])
+def get_term_by_name(term_name):
+    term = Term.query.filter_by(term_string=term_name).first()
+    if term:
+        return jsonify({
+            'id': term.id,
+            'term_string': term.term_string,
+            'definition': term.definition
+        })
+    else:
+        return jsonify({'error': 'Term not found'}), 404
