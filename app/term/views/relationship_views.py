a68f3dc3079a147c505779d362d4c88cc915f3b4
cr625 <cr625@drexel.edu>
2024-12-08 20:19:53 -0500

    formatting

--- app/term/views/relationship_views.py
+++ app/term/views/relationship_views.py
@@ -13,7 +13,8 @@ def display_relation(output):
 
 def get_terms_with_predicate_tag():
     ontology_terms = Term.query.filter(
-        Term.tags.any(Tag.value == "rdfs:predicate")).all()
+        Term.tags.any(Tag.value == "rdfs:predicate")
+    ).all()
     return ontology_terms
 
 
@@ -21,19 +22,23 @@ def get_terms_with_predicate_tag():
 @login_required
 def add_relationship():
     term_list = get_terms_with_predicate_tag()
-    choices = [('', 'Select a relationship')] + \
-        [(term.concept_id, term.term_string) for term in term_list]
+    choices = [("", "Select a relationship")] + [
+        (term.concept_id, term.term_string) for term in term_list
+    ]
 
     relationship_form = AddRelationshipForm()
     relationship_form.predicate_id.choices = choices
 
     if relationship_form.validate_on_submit():
         subject = Term.query.filter_by(
-            concept_id=relationship_form.subject_id.data).first()
+            concept_id=relationship_form.subject_id.data
+        ).first()
         predicate = Term.query.filter_by(
-            concept_id=relationship_form.predicate_id.data).first()
+            concept_id=relationship_form.predicate_id.data
+        ).first()
         object = Term.query.filter_by(
-            concept_id=relationship_form.object_id.data).first()
+            concept_id=relationship_form.object_id.data
+        ).first()
 
         if not subject or not predicate or not object:
             flash("Error: One or more terms could not be found.", "danger")
@@ -47,12 +52,14 @@ def add_relationship():
             predicate_id=predicate.id,
             child_id=object.id,
             ark_id=ark.id,
-            owner_id=current_user.id
+            owner_id=current_user.id,
         )
         new_relationship.save()
 
         return subject.term_string + predicate.term_string + object.term_string
-    return render_template("relationship/add_relationship.jinja", form=relationship_form)
+    return render_template(
+        "relationship/add_relationship.jinja", form=relationship_form
+    )
 
 
 @term.route("/relationship/edit/<int:relationship_id>", methods=["GET", "POST"])
@@ -60,47 +67,63 @@ def add_relationship():
 def edit_relationship(relationship_id):
     relationship = Relationship.query.get_or_404(relationship_id)
     term_list = get_terms_with_predicate_tag()
-    choices = [('', 'Select a relationship')] + \
-        [(term.concept_id, term.term_string) for term in term_list]
+    choices = [("", "Select a relationship")] + [
+        (term.concept_id, term.term_string) for term in term_list
+    ]
 
     relationship_form = EditRelationshipForm()
     relationship_form.predicate_id.choices = choices
 
-    if request.method == 'GET':
+    if request.method == "GET":
         subject_term = Term.query.get(relationship.child_id)
         predicate_term = Term.query.get(relationship.predicate_id)
         object_term = Term.query.get(relationship.parent_id)
 
-        relationship_form.subject_term_string.data = subject_term.term_string if subject_term else ''
+        relationship_form.subject_term_string.data = (
+            subject_term.term_string if subject_term else ""
+        )
         relationship_form.predicate_id.data = relationship.predicate_id
-        relationship_form.object_term_string.data = object_term.term_string if object_term else ''
+        relationship_form.object_term_string.data = (
+            object_term.term_string if object_term else ""
+        )
 
     if relationship_form.validate_on_submit():
         subject = Term.query.filter_by(
-            concept_id=relationship_form.subject_id.data).first()
+            concept_id=relationship_form.subject_id.data
+        ).first()
         predicate = Term.query.filter_by(
-            concept_id=relationship_form.predicate_id.data).first()
-        obj = Term.query.filter_by(
-            concept_id=relationship_form.object_id.data).first()
+            concept_id=relationship_form.predicate_id.data
+        ).first()
+        obj = Term.query.filter_by(concept_id=relationship_form.object_id.data).first()
 
         if not subject or not predicate or not object:
             flash("Error: One or more terms could not be found.", "danger")
-            return redirect(url_for("term.edit_relationship", relationship_id=relationship_id))
+            return redirect(
+                url_for("term.edit_relationship", relationship_id=relationship_id)
+            )
 
         relationship.child_id = subject.id
         relationship.predicate_id = predicate.id
         relationship.parent_id = object.id
         relationship.save()
 
-        return redirect(url_for("term.display_relationship", relationship_id=relationship_id))
+        return redirect(
+            url_for("term.display_relationship", relationship_id=relationship_id)
+        )
 
-    return render_template("relationship/edit_relationship.jinja", form=relationship_form, relationship=relationship)
+    return render_template(
+        "relationship/edit_relationship.jinja",
+        form=relationship_form,
+        relationship=relationship,
+    )
 
 
 @term.route("/relationship/display/<int:relationship_id>")
 def display_relationship(relationship_id):
     relationship = Relationship.query.get_or_404(relationship_id)
-    return render_template("relationship/display_relationship.jinja", relationship=relationship)
+    return render_template(
+        "relationship/display_relationship.jinja", relationship=relationship
+    )
 
 
 @term.route("/relationship/delete/<int:relationship_id>", methods=["POST"])
@@ -110,10 +133,14 @@ def delete_relationship(relationship_id):
     term_set_id = request.args.get("term_set_id")
     relationship.delete()
     flash("Relationship deleted.")
-    return redirect(url_for("term.display_termset", term_set_id=term_set_id, tab="relationships"))
+    return redirect(
+        url_for("term.display_termset", term_set_id=term_set_id, tab="relationships")
+    )
 
 
 @term.route("/relationship/list/")
 def list_properties():
     relationships = Relationship.query.all()
-    return render_template("relationship/list_properties.jinja", relationships=relationships)
+    return render_template(
+        "relationship/list_properties.jinja", relationships=relationships
+    )
--- app/term/views/termset_views.py
+++ app/term/views/termset_views.py
@@ -1,68 +1,103 @@
 import os
-from flask import (flash, redirect, render_template,
-                   send_file, url_for, request)
+from flask import (
+    flash,
+    redirect,
+    render_template,
+    send_file,
+    url_for,
+    request,
+    current_app,
+)
 from flask_login import current_user, login_required
 
+# Import Blueprints, Forms, Models, and Utilities
 from app.term import term_blueprint as term
 from app.term.forms import EmptyForm, AddTagForm, EditTermSetForm, AddSubClassForm
 from app.term.models import TermSet, Tag, Term, Ark, Relationship
-from app.user.models import User
-from app.term.views.list_views import *
 from app.term.models.association_tables import termset_relationships
-
+from app.term.views.list_views import *
 from app.io.ontology import OntologyClassifier
+from app.user.models import User
+from typing import Dict, Any, List, Union
+from flask import Response
 
+# Utility Functions
+def get_hierarchy_data(term_set_id: int) -> Dict[str, Any]:
+    """
+    Builds a hierarchical structure for the given term set based on SubClass
+    <Object(Child)> <Predicate:isSubClassOf> <Subject(Parent) using networkx.
 
-def get_hierarchy_data(term_set_id):
+    Args:
+        term_set_id (int): The ID of the term set.
+
+    Returns:
+        dict: A nested dictionary representing the hierarchy.
+    """
     term_set = TermSet.query.get_or_404(term_set_id)
     classifier = OntologyClassifier(term_set)
     hierarchy = classifier.build_hierarchy()
 
-    # Identify a root node (a node with no incoming edges)
     root = [node for node, degree in hierarchy.in_degree() if degree == 0]
     if not root:
         flash("No root node found for the hierarchy.")
         return redirect(url_for("term.display_termset", term_set_id=term_set_id))
 
-    # Convert the hierarchy to a nested dictionary format
     def build_tree(node):
         children = list(hierarchy.successors(node))
         return {
             "name": node,
-            "definition": hierarchy.nodes[node].get('definition', 'No definition available'),
-            "children": [build_tree(child) for child in children]
+            "definition": hierarchy.nodes[node].get(
+                "definition", "No definition available"
+            ),
+            "children": [build_tree(child) for child in children],
         }
-    hierarchy_data = build_tree(root[0])
-    return hierarchy_data
+
+    return build_tree(root[0])
 
 
-def populate_relationships(relationships, term_set):
+def populate_relationships(relationships: List[Relationship], term_set: TermSet) -> List[Relationship]:
+    """
+    Populates the relationships with their associated terms, owners, and ARKs.
+
+    Args:
+        relationships (List[Relationship]): The list of relationships to populate.
+        term_set (TermSet): The term set to which the relationships belong.
+
+    Returns:
+        List[Relationship]: The list or relationships that turns the id keys into their model representation.
+    """
     for relationship in relationships:
         relationship.parent = Term.query.get(relationship.parent_id)
         relationship.predicate = Term.query.get(relationship.predicate_id)
         relationship.child = Term.query.get(relationship.child_id)
         relationship.owner = User.query.get(relationship.owner_id)
         relationship.ark = Ark.query.get(relationship.ark_id)
-        relationship.termset_name = term_set.name  # we alrady know this.
+
     return relationships
 
 
+def log_form_errors(form: Any) -> None:
+    for field, errors in form.errors.items():
+        for error in errors:
+            current_app.logger.error(f"Error in {field}: {error}")
+
+
+# Route Handlers
 @term.route("/set/list")
-def list_termsets():
+def list_termsets() -> str:
     term_sets = TermSet.query.order_by(TermSet.name)
     return render_template("termset/list_termsets.jinja", term_sets=term_sets)
 
 
 @term.route("/set/display/<int:term_set_id>")
-def display_termset(term_set_id):
+def display_termset(term_set_id: int) -> str:
     term_set = TermSet.query.get_or_404(term_set_id)
     tag_form = AddTagForm()
     add_subclass_form = AddSubClassForm()
-    tag_form.tag_list.choices = [(tag.id, tag.value)
-                                 for tag in Tag.query.order_by(Tag.value).all()]
-
+    tag_form.tag_list.choices = [
+        (tag.id, tag.value) for tag in Tag.query.order_by(Tag.value).all()
+    ]
     hierarchy_data = get_hierarchy_data(term_set_id)
-
     relationships = populate_relationships(term_set.relationships, term_set)
 
     return render_template(
@@ -72,65 +107,60 @@ def display_termset(term_set_id):
         tag_form=tag_form,
         subclass_form=add_subclass_form,
         relationships=relationships,
-        hierarchy=hierarchy_data
+        hierarchy=hierarchy_data,
     )
 
 
 @term.route("/set/add_subclass/<int:term_set_id>", methods=["POST"])
 @login_required
-def add_subclass(term_set_id):
+def add_subclass(term_set_id: int) -> Response:
     term_set = TermSet.query.get_or_404(term_set_id)
-    add_subclass_form = AddSubClassForm()
+    form = AddSubClassForm()
 
-    # get the rdfs:subClassOf term
     rdfs_subclass = Term.query.filter_by(term_string="rdfs:subClassOf").first()
-    child_term = Term.query.filter_by(
-        concept_id=add_subclass_form.child_id.data).first()
+    child_term = Term.query.filter_by(concept_id=form.child_id.data).first()
+
     if not child_term:
         flash("Error: Child term not found.")
-        return redirect(url_for("term.display_termset", term_set_id=term_set_id, tab="classes"))
+        return redirect(
+            url_for("term.display_termset", term_set_id=term_set_id, tab="classes")
+        )
 
-    if add_subclass_form.validate_on_submit():
+    if form.validate_on_submit():
         relationship = Relationship(
-            parent_id=add_subclass_form.parent_term_id.data,
+            parent_id=form.parent_term_id.data,
             predicate_id=rdfs_subclass.id,
             child_id=child_term.id,
             owner_id=current_user.id,
             ark_id=Ark().create_ark(shoulder="g1", naan="13183").id,
             created=db.func.now(),
-            modified=db.func.now()
+            modified=db.func.now(),
         )
 
-        # Add the specific tag to the relationship
         subclass_tag = Tag.query.filter_by(value="subclass_added").first()
         if subclass_tag:
             relationship.tags.append(subclass_tag)
+
         db.session.add(relationship)
-        db.session.commit()
-
-        # Append the relationship to the term set
         term_set.relationships.append(relationship)
-        db.session.commit()
 
-        # Add the child term to the term set if not already a member
         if child_term not in term_set.terms:
             term_set.terms.append(child_term)
-            db.session.commit()
 
+        db.session.commit()
         flash("Subclass added successfully.")
     else:
-        # Log form errors
-        for field, errors in add_subclass_form.errors.items():
-            for error in errors:
-                current_app.logger.error(f"Error in {field}: {error}")
+        log_form_errors(form)
         flash("Error: Form validation failed.")
 
-    return redirect(url_for("term.display_termset", term_set_id=term_set_id, tab="classes"))
+    return redirect(
+        url_for("term.display_termset", term_set_id=term_set_id, tab="classes")
+    )
 
 
 @term.route("/set/edit/<int:term_set_id>", methods=["GET", "POST"])
 @login_required
-def edit_termset(term_set_id):
+def edit_termset(term_set_id: int) -> Union[str, Response]:
     term_set = TermSet.query.get_or_404(term_set_id)
     form = EditTermSetForm(obj=term_set)
 
@@ -142,17 +172,14 @@ def edit_termset(term_set_id):
     else:
         if request.method == "POST":
             flash("Error: Form validation failed.")
-            for field, errors in form.errors.items():
-                for error in errors:
-                    flash(
-                        f"Error in {getattr(form, field).label.text}: {error}")
+            log_form_errors(form)
 
     return render_template("termset/edit_termset.jinja", form=form, term_set=term_set)
 
 
 @term.route("/set/delete/<int:term_set_id>", methods=["POST"])
 @login_required
-def delete_term_set(term_set_id):
+def delete_term_set(term_set_id: int) -> Response:
     term_set = TermSet.query.get_or_404(term_set_id)
     if term_set.user_id == current_user.id or current_user.is_administrator:
         db.session.delete(term_set)
@@ -165,96 +192,92 @@ def delete_term_set(term_set_id):
 
 @term.route("/set/add_tag/<int:term_set_id>", methods=["POST"])
 @login_required
-def add_tag_to_term_set(term_set_id):
+def add_tag_to_term_set(term_set_id: int) -> Response:
     term_set = TermSet.query.get_or_404(term_set_id)
     tag_form = AddTagForm()
-    tag_form.tag_list.choices = [('', 'Select a tag')] + [(tag.id, tag.value)
-                                                          for tag in Tag.query.order_by(Tag.value).all()]
+    tag_form.tag_list.choices = [
+        (tag.id, tag.value) for tag in Tag.query.order_by(Tag.value).all()
+    ]
 
     if tag_form.validate_on_submit():
-        if tag_form.tag_list.data == '':
-            flash("Please select a valid tag.")
+        tag = Tag.query.get(tag_form.tag_list.data)
+        if tag and tag not in term_set.tags:
+            term_set.tags.append(tag)
+            db.session.commit()
+            flash("Tag added.")
         else:
-            tag = Tag.query.get(tag_form.tag_list.data)
-            if tag:
-                if tag not in term_set.tags:
-                    term_set.tags.append(tag)
-                    db.session.commit()
-                    flash("Tag added.")
-                else:
-                    flash("Tag already exists in the term set.")
-            else:
-                flash("Invalid tag selected.")
+            flash("Tag already exists in the term set.")
+    else:
+        flash("Error: Form validation failed.")
+
     return redirect(url_for("term.display_termset", term_set_id=term_set_id))
 
 
 @term.route("/set/remove_tag/<int:term_set_id>/<int:tag_id>", methods=["POST"])
 @login_required
-def remove_tag_from_term_set(term_set_id, tag_id):
-    form = EmptyForm()
+def remove_tag_from_term_set(term_set_id: int, tag_id: int) -> Response:
     term_set = TermSet.query.get_or_404(term_set_id)
     tag = Tag.query.get_or_404(tag_id)
+
     if tag in term_set.tags:
         term_set.tags.remove(tag)
         db.session.commit()
         flash("Tag removed.")
     else:
         flash("Tag not found in term set.")
+
     return redirect(url_for("term.display_termset", term_set_id=term_set_id))
 
 
 @term.route("/set/download_file/<filename>", methods=["GET"])
 @login_required
-def download_file(filename):
-    import_dir = os.path.join(current_app.root_path, 'io', 'import')
+def download_file(filename: str) -> Union[Response, str]:
+    import_dir = os.path.join(current_app.root_path, "io", "import")
     file_path = os.path.join(import_dir, filename)
-
-    # Log the file path for debugging
-    current_app.logger.debug(f"Looking for file at: {file_path}")
 
     if os.path.exists(file_path):
         return send_file(file_path, as_attachment=True)
     else:
         flash("File not found.")
-        return redirect(url_for("term.list_terms"))
+        return redirect(url_for("term.list_termsets"))
 
 
 @term.route("/set/classify_ontology/<int:term_set_id>")
 @login_required
-def classify_ontology(term_set_id):
+def classify_ontology(term_set_id: int) -> str:
     term_set = TermSet.query.get_or_404(term_set_id)
     classifier = OntologyClassifier(term_set)
     relationships = classifier.create_relationships()
-
-    # Fetch term strings for relationships
     new_relationships = populate_relationships(relationships, term_set)
 
     flash("OntologyClassifier completed.")
-    return render_template("termset/list_relations.jinja", relationships=new_relationships)
+    return render_template(
+        "termset/list_relations.jinja", relationships=new_relationships
+    )
 
 
 @term.route("/set/relationships/<int:term_set_id>")
 @login_required
-def list_termset_relationships(term_set_id):
+def list_termset_relationships(term_set_id: int) -> str:
     term_set = TermSet.query.get_or_404(term_set_id)
-    relationships = term_set.relationships
-
-    relationships = populate_relationships(relationships, term_set)
+    relationships = populate_relationships(term_set.relationships, term_set)
 
     return render_template("termset/list_relations.jinja", relationships=relationships)
 
 
 @term.route("/set/classes/display/<int:term_set_id>")
-def display_classes(term_set_id):
+def display_classes(term_set_id: int) -> str:
     term_set = TermSet.query.get_or_404(term_set_id)
     hierarchy_data = get_hierarchy_data(term_set_id)
 
-    return render_template("termset/display_classes.jinja", hierarchy=hierarchy_data, term_set=term_set)
+    return render_template(
+        "termset/display_classes.jinja", hierarchy=hierarchy_data, term_set=term_set
+    )
 
 
 @term.route("/set/copy/<int:term_set_id>", methods=["POST"])
 @login_required
-def copy_termset(term_set_id):
+def copy_termset(term_set_id: int) -> Response:
     original_term_set = TermSet.query.get_or_404(term_set_id)
     new_term_set = TermSet(
         name=f"Copy of {original_term_set.name}",
@@ -262,18 +285,12 @@ def copy_termset(term_set_id):
         source=original_term_set.source,
         user_id=current_user.id,
         created=db.func.now(),
-        updated=db.func.now()
+        updated=db.func.now(),
     )
     db.session.add(new_term_set)
-    db.session.commit()
 
-    # Copy tags
-    for tag in original_term_set.tags:
-        new_term_set.tags.append(tag)
-
-    # Copy terms
-    for term in original_term_set.terms:
-        new_term_set.terms.append(term)
+    new_term_set.tags.extend(original_term_set.tags)
+    new_term_set.terms.extend(original_term_set.terms)
 
     db.session.commit()
     flash("Term set copied successfully.")
@@ -281,10 +298,6 @@ def copy_termset(term_set_id):
 
 
 @term.route("/set/simple/<int:term_set_id>")
-def display_simple_termset(term_set_id):
+def display_simple_termset(term_set_id: int) -> str:
     term_set = TermSet.query.get_or_404(term_set_id)
-
-    return render_template(
-        "termset/display_simple_termset.jinja",
-        term_set=term_set,
-    )
+    return render_template("termset/display_simple_termset.jinja", term_set=term_set)
